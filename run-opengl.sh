2d29228960738b98942cd1c04c89b10c3996047e
Arovill1 <arovill1@VDID026-UQAC-13.ad-urca.univ-reims.fr>
2023-06-16 22:22:19 +0200

    docker + cmake + pain

--- run-opengl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export DISPLAY=:0  # Définit le numéro de l'affichage virtuel (peut être différent selon votre système)
-xvfb-run -a -s "-screen 0 1280x1024x24" ./NBodyGraphics/NBodyGraphics
