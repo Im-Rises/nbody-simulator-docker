9e15d8342299ad33e763f2f6f8d8bde2c7af2d1e
Arovill1 <arovill1@VDID026-UQAC-13.ad-urca.univ-reims.fr>
2023-06-20 14:30:44 +0200

    add command to stop all containers in the test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,9 @@
 
 start_time=$(date +%s)  # Enregistre le temps de départ en secondes depuis l'époque Unix
 
+docker container stop $(docker container ls -q)
 docker-compose down
-docker-compose up -d --build
+docker-compose up -d #--build
 docker logs -f nbody-simulator-docker_opengl_1
 
 end_time=$(date +%s)    # Enregistre le temps d'arrêt en secondes depuis l'époque Unix
