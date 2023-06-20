3ade96b277f995698c11c7f2d17fa771433494b0
Im-Rises <quentin-morel88@hotmail.com>
2023-06-20 09:48:39 -0400

    Corrected bash

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 start_time=$(date +%s)  # Enregistre le temps de départ en secondes depuis l'époque Unix
 
-docker container stop $(docker container ls -q)
-docker-compose down
-docker-compose up -d #--build
+docker container stop "$(docker container ls -q)"
+docker-compose down --remove-orphans
+docker-compose up -d --build
 docker logs -f nbody-simulator-docker_opengl_1
 
 end_time=$(date +%s)    # Enregistre le temps d'arrêt en secondes depuis l'époque Unix
@@ -12,3 +12,8 @@ end_time=$(date +%s)    # Enregistre le temps d'arrêt en secondes depuis l'épo
 duration=$((end_time - start_time))  # Calcule la durée en secondes
 
 echo "Le chronomètre a fonctionné pendant $duration secondes."
+
+echo "Clearing docker containers..."
+docker-compose down --remove-orphans
+
+echo "Exiting..."
